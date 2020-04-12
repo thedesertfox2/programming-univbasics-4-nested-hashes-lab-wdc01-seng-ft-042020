ef8afe2eed11506fd9825e48df007f7908e7dbee
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-12 15:37:33 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,10 +52,10 @@ def dennis_ritchies_language
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => "C"
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -81,7 +81,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+   programmer_hash = {
+     :yukihiro_matsumoto => {
+       :known_for => "Ruby",
+       :languages => ["LISP", "C"]
+     }
+   }
 end
 
 def changing_alan
@@ -89,6 +94,7 @@ def changing_alan
   # return the entire updated hash
 
 	programmer_hash = 
+ 		
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -103,7 +109,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +132,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  programmer_hash
 end
