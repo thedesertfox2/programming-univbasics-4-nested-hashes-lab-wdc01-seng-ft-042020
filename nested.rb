e3b476d31d7c5f3931761083a1e2f4d266461dd8
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-12 15:30:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,7 +36,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for]
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def dennis_ritchies_language
